0d5d043c2990c664efcb7339555c12cb2911432b
Roman Suvorov <suvorov83@gmail.com>
2021-10-21 09:33:11 +0300

    pickAttributesNames tests

--- internal/processors/tags/tags_test.go
+++ internal/processors/tags/tags_test.go
@@ -577,3 +577,53 @@ func TestNewProcessor(t *testing.T) {
 		rq.Equal(domain.TagList, p.query.searchType)
 	})
 }
+
+func TestPickAttributesNames(t *testing.T) {
+	t.Parallel()
+
+	t.Run("nil: empty tag", func(t *testing.T) {
+		t.Parallel()
+
+		tag := []byte{}
+		res := pickAttributesNames(tag)
+
+		rq := require.New(t)
+		rq.Nil(res)
+	})
+
+	t.Run("nil: first byte is not an open bracket", func(t *testing.T) {
+		t.Parallel()
+
+		tag := []byte("tagname")
+		res := pickAttributesNames(tag)
+
+		rq := require.New(t)
+		rq.Nil(res)
+	})
+
+	t.Run("ok", func(t *testing.T) {
+		t.Parallel()
+
+		tag := []byte("<tagname attr1='value1' attr2='value2' attr3='value3'")
+		res := pickAttributesNames(tag)
+
+		rq := require.New(t)
+		rq.Len(res, 3)
+		rq.Equal("attr1", res[0])
+		rq.Equal("attr2", res[1])
+		rq.Equal("attr3", res[2])
+	})
+
+	t.Run("ok: other quotes", func(t *testing.T) {
+		t.Parallel()
+
+		tag := []byte(`<tagname attr1="value1" attr2="value2" attr3="value3"`)
+		res := pickAttributesNames(tag)
+
+		rq := require.New(t)
+		rq.Len(res, 3)
+		rq.Equal("attr1", res[0])
+		rq.Equal("attr2", res[1])
+		rq.Equal("attr3", res[2])
+	})
+}
