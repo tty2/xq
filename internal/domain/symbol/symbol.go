07bf2d75929f2adc6fb1997da4817931dda0a11f
Roman Suvorov <suvorov83@gmail.com>
2022-05-11 10:30:36 +0300

    add comment

--- internal/domain/symbol/symbol.go
+++ internal/domain/symbol/symbol.go
@@ -22,6 +22,7 @@ func IsQuote(s byte) bool {
 	return s == Quote || s == DoubleQuote
 }
 
+// IsSpace checks is byte is space.
 func IsSpace(s byte) bool {
 	if s == ' ' || s == '\n' || s == '\t' {
 		return true
